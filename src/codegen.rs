ed36da88c1f1904185b47b31761db154bf288d42
Francisco Giordano <fg@frang.io>
2024-05-20 14:36:06 -0300

    add spill requirement tracking

--- src/codegen.rs
+++ src/codegen.rs
@@ -47,161 +47,182 @@ enum VarInstance {
     Copy(Var),
 }
 
-struct VarLocation {
+impl VarInstance {
+    fn name(&self) -> Var {
+        use VarInstance::*;
+        match self {
+            Main(name) | Copy(name) => *name
+        }
+    }
+}
+
+#[derive(PartialEq, Eq)]
+enum SpillStatus {
+    Unopened,
+    Opened,
+    Closed,
+}
+
+#[derive(Clone, Copy)]
+struct Spill {
+    code_index: usize,
+    stack_depth: usize,
+}
+
+impl Spill {
+    fn new(code: &Vec<Instruction>, stack_depth: usize) -> Spill {
+        Spill {
+            code_index: code.len() - 1,
+            stack_depth,
+        }
+    }
+}
+
+struct VarMeta {
     main_index: usize,
-    copy_index_plus1: Option<NonZeroUsize>,
-}
-
-impl VarLocation {
-    fn new(index: usize) -> VarLocation {
-        VarLocation { main_index: index, copy_index_plus1: None }
-    }
-
-    fn get_copy_index(&self) -> Option<usize> {
-        self.copy_index_plus1.map(|i| i.get() - 1)
-    }
-
-    fn set_copy_index(&mut self, i: Option<usize>) {
-        self.copy_index_plus1 = i.and_then(|i| NonZeroUsize::new(i + 1))
-    }
-}
-
-fn set_location(location: &mut HashMap<Var, VarLocation>, instance: VarInstance, index: Option<usize>) {
-    match instance {
-        VarInstance::Main(name) => {
-            if let Some(index) = index {
-                location.get_mut(&name).unwrap().main_index = index;
-            } else {
-                location.remove(&name).unwrap();
-            }
-        },
-        VarInstance::Copy(name) => {
-            location.get_mut(&name).unwrap().set_copy_index(index);
-        },
-    }
-}
-
-struct Data {
-    stack: Vec<VarInstance>,
-    location: HashMap<Var, VarLocation>,
-}
-
-impl Data {
-    fn new() -> Data {
-        Data {
-            stack: Vec::new(),
-            location: HashMap::new(),
-        }
-    }
-
-    fn find_depth(&self, name: Var) -> usize {
-        let loc = self.location.get(&name).unwrap();
-        let index = loc.get_copy_index().unwrap_or(loc.main_index);
-        self.stack.len() - 1 - index
-    }
-
-    fn pop(&mut self) {
-        let instance = self.stack.pop().unwrap();
-        set_location(&mut self.location, instance, None);
-    }
-
-    fn push(&mut self, name: Var) {
-        self.stack.push(VarInstance::Main(name));
-        let index = self.stack.len() - 1;
-        self.location.insert(name, VarLocation::new(index));
-    }
-
-    fn copy(&mut self, name: Var) {
-        let instance = VarInstance::Copy(name);
-        self.stack.push(instance);
-        let index = self.stack.len() - 1;
-        set_location(&mut self.location, instance, Some(index));
-    }
-
-    fn swap(&mut self, from_depth: usize, to_depth: usize) {
-        let top = self.stack.len() - 1;
-        let from_index = top - from_depth;
-        let to_index = top - to_depth;
-        let from_instance = self.stack[from_index];
-        let to_instance = self.stack[to_index];
-        self.stack.swap(from_index, to_index);
-        set_location(&mut self.location, from_instance, Some(to_index));
-        set_location(&mut self.location, to_instance, Some(from_index));
-    }
-
-    fn drain(&mut self, count: usize) {
-        for instance in self.stack.drain(self.stack.len() - count..) {
-            set_location(&mut self.location, instance, None);
-        }
-    }
-
-    fn extend(&mut self, names: &[Var]) {
-        let first = self.stack.len();
-        let instances = names.iter().enumerate().map(|(i, &name)| {
-            self.location.insert(name, VarLocation::new(first + i));
-            VarInstance::Main(name)
-        });
-        self.stack.extend(instances);
-    }
+    copy_index: Option<usize>,
+    spill_status: SpillStatus,
+    spill: Spill,
 }
 
 struct Machine {
     code: Vec<Instruction>,
-    data: Data,
+    stack: Vec<VarInstance>,
+    meta: HashMap<Var, VarMeta>,
+    spills: Vec<(Spill, bool)>,
 }
 
 impl Machine {
     fn new() -> Machine {
         Machine {
             code: Vec::new(),
-            data: Data::new(),
+            stack: Vec::new(),
+            meta: HashMap::new(),
+            spills: Vec::new(),
         }
+    }
+
+    fn set_location(&mut self, instance: VarInstance, index: Option<usize>) {
+        match instance {
+            VarInstance::Main(name) => {
+                if let Some(index) = index {
+                    self.meta.get_mut(&name).unwrap().main_index = index;
+                } else {
+                    let meta = self.meta.remove(&name).unwrap();
+                    if meta.spill_status != SpillStatus::Unopened {
+                        self.spills.push((meta.spill, false));
+                    }
+                }
+            },
+            VarInstance::Copy(name) => {
+                self.meta.get_mut(&name).unwrap().copy_index = index;
+            },
+        }
+    }
+
+    fn set_spill(&mut self, from_depth: usize, name: Var, spill: Spill) {
+        let meta = self.meta.get_mut(&name).unwrap();
+        if from_depth > 16 {
+            if meta.spill_status == SpillStatus::Unopened {
+                self.spills.push((meta.spill, true));
+            }
+            meta.spill_status = SpillStatus::Opened;
+            meta.spill = spill;
+        } else if meta.spill_status != SpillStatus::Closed {
+            meta.spill_status = SpillStatus::Closed;
+            meta.spill = spill;
+        }
+    }
+
+    fn find_depth(&self, name: Var) -> usize {
+        let meta = self.meta.get(&name).unwrap();
+        let index = meta.copy_index.unwrap_or(meta.main_index);
+        self.stack.len() - 1 - index
     }
 
     fn pop(&mut self) {
-        self.data.pop();
+        let instance = self.stack.pop().unwrap();
         self.code.push(Instruction::Data(DataInstruction::Pop));
+        self.set_location(instance, None);
     }
 
     fn push(&mut self, name: Var, value: U256) {
-        self.data.push(name);
+        self.stack.push(VarInstance::Main(name));
         self.code.push(Instruction::Stack(StackInstruction::Push(value.into())));
+        self.meta.insert(name, VarMeta {
+            main_index: self.stack.len() - 1,
+            copy_index: None,
+            spill_status: SpillStatus::Unopened,
+            spill: Spill::new(&self.code, 0),
+        });
     }
 
-    fn swap_to(&mut self, from_name: Var, to_depth: usize) -> usize {
+    fn stack_swap(&mut self, from_depth: usize, to_depth: usize) -> VarInstance {
+        let stack_top = self.stack.len() - 1;
+        let from_index = stack_top - from_depth;
+        let to_index = stack_top - to_depth;
+        let from_instance = self.stack[from_index];
+        let to_instance = self.stack[to_index];
+        self.stack.swap(from_index, to_index);
+        self.set_location(from_instance, Some(to_index));
+        self.set_location(to_instance, Some(from_index));
+        to_instance
+    }
+
+    fn swap_to(&mut self, from_name: Var, to_depth: usize) {
         assert!(to_depth <= 16, "Swap too deep");
-        let from_depth = self.data.find_depth(from_name);
+        let from_depth = self.find_depth(from_name);
         if from_depth != to_depth {
-            self.data.swap(from_depth, to_depth);
-            if from_depth != 0 {
-                self.code.push(Instruction::Stack(StackInstruction::Swap(from_depth)));
-            }
-            if to_depth != 0 {
-                self.code.push(Instruction::Stack(StackInstruction::Swap(to_depth)));
-            }
+            self.stack_swap(from_depth, 0);
+            self.code.push(Instruction::Stack(StackInstruction::Swap(from_depth)));
+            self.set_spill(from_depth, from_name, Spill::new(&self.code, 0));
+
+            let to_instance = self.stack_swap(0, to_depth);
+            self.code.push(Instruction::Stack(StackInstruction::Swap(to_depth)));
+            self.set_spill(to_depth, to_instance.name(), Spill::new(&self.code, 0))
         }
-        from_depth
     }
 
-    fn copy_to(&mut self, from_name: Var, to_depth: usize) -> usize {
+    fn copy_to(&mut self, from_name: Var, to_depth: usize) {
         assert!(to_depth <= 16, "Copy too deep");
-        let from_depth = self.data.find_depth(from_name);
-        self.data.copy(from_name);
+        let from_depth = self.find_depth(from_name);
+
+        let instance = VarInstance::Copy(from_name);
+        self.stack.push(instance);
+        let index = self.stack.len() - 1;
         self.code.push(Instruction::Stack(StackInstruction::Dup(from_depth)));
+        self.set_location(instance, Some(index));
+        self.set_spill(from_depth, from_name, Spill::new(&self.code, 0));
+
         if to_depth != 0 {
-            self.data.swap(0, to_depth);
-            if to_depth != from_depth + 1 {
-                self.code.push(Instruction::Stack(StackInstruction::Swap(to_depth)));
-            }
+            let to_instance = self.stack_swap(0, to_depth);
+
+            self.code.push(Instruction::Stack(StackInstruction::Swap(to_depth)));
+            self.set_spill(to_depth, to_instance.name(), Spill::new(&self.code, 0));
         }
-        from_depth
     }
 
     fn apply(&mut self, op: DataInstruction, ress: &[Var]) {
-        let (nargs, _) = op.arity();
-        self.data.drain(nargs);
-        self.data.extend(ress);
+        let (nargs, nress) = op.arity();
+        let stack_base = self.stack.len() - nargs;
+
+        let removed = self.stack.split_off(stack_base);
+        for &instance in &removed {
+            self.set_location(instance, None);
+        }
+        self.stack.extend(ress.iter().map(|&name| VarInstance::Main(name)));
+
         self.code.push(Instruction::Data(op));
+
+        for (i, &name) in ress.iter().enumerate() {
+            let depth = nress - 1 - i;
+            self.meta.insert(name, VarMeta {
+                main_index: stack_base + i,
+                copy_index: None,
+                spill_status: SpillStatus::Unopened,
+                spill: Spill::new(&self.code, depth),
+            });
+        }
     }
 }
 
@@ -228,16 +249,14 @@ pub fn generate(rblock: &ResolvedBlock) -> Result<impl Iterator<Item=impl Into<e
                 let dups: Vec<_> = args.iter()
                     .map(|&a| {
                         occurs[a.index()] -= 1;
-                        let d = occurs[a.index()] > 0;
-                        if d { ndups += 1; }
-                        d
+                        let dup = occurs[a.index()] > 0;
+                        if dup { ndups += 1; }
+                        dup
                     })
                     .collect();
 
                 for (i, (&arg, dup)) in args.iter().zip(dups).enumerate().rev() {
-                    if dup {
-                        ndups -= 1;
-                    }
+                    if dup { ndups -= 1; }
                     let to_depth = i - ndups;
                     if dup {
                         machine.copy_to(arg, to_depth);
